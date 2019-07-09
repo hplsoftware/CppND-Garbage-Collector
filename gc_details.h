5577fe8647ecd110e89a70b9729ba182123786b0
Rob Sutherland <rob.s@hplsoftware.com>
2019-07-08 22:52:30 -0600

    Update gc_details.h
    taken from my udacity workspace after successful compile

--- gc_details.h
+++ gc_details.h
@@ -4,30 +4,48 @@
 template <class T>
 class PtrDetails
 {
-  public:
-    unsigned refcount; // current reference count
-    T *memPtr;         // pointer to allocated memory
-    /* isArray is true if memPtr points
+public:
+	unsigned refcount; // current reference count
+	T *memPtr;         // pointer to allocated memory
+	/* isArray is true if memPtr points
 to an allocated array. It is false
 otherwise. */
-    bool isArray; // true if pointing to array
-    /* If memPtr is pointing to an allocated
+	bool isArray; // true if pointing to array
+	/* If memPtr is pointing to an allocated
 array, then arraySize contains its size */
-    unsigned arraySize; // size of array
-    // Here, mPtr points to the allocated memory.
-    // If this is an array, then size specifies
-    // the size of the array.
+	unsigned arraySize; // size of array
+	// Here, mPtr points to the allocated memory.
+	// If this is an array, then size specifies
+	// the size of the array.
 
-    PtrDetails(void)
-    {
-        // TODO: Implement PtrDetails
-    }
+	PtrDetails(T* _pnt, int _size = 0) :memPtr(_pnt), arraySize(_size)
+	{
+		//DONE
+
+		//initially set to false
+		isArray = false;
+
+		//set mem pointer to passed in val
+		memPtr = _pnt;
+
+		//set size
+		arraySize = _size;
+
+		//initialize the refcount var
+		refcount = 1;
+
+		//if the passed in size is valid, set the array existence
+		//as being true
+		if (_size > 0)
+			isArray = true;
+	}
 };
 // Overloading operator== allows two class objects to be compared.
 // This is needed by the STL list class.
 template <class T>
 bool operator==(const PtrDetails<T> &ob1,
-                const PtrDetails<T> &ob2)
+	const PtrDetails<T> &ob2)
 {
-    // TODO: Implement operator==
+	// DONE
+	return (ob1.memPtr == ob2.memPtr);
 }

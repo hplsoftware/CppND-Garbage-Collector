b7c7c9b35eb31935a6affa5079c3fb94fb2658c4
Rob Sutherland <rob.s@hplsoftware.com>
2019-07-15 16:17:58 -0600

    bug fixes

--- gc_details.h
+++ gc_details.h
@@ -7,45 +7,46 @@ class PtrDetails
 public:
 	unsigned refcount; // current reference count
 	T *memPtr;         // pointer to allocated memory
-	/* isArray is true if memPtr points
-to an allocated array. It is false
-otherwise. */
+					   /* isArray is true if memPtr points
+					   to an allocated array. It is false
+					   otherwise. */
 	bool isArray; // true if pointing to array
-	/* If memPtr is pointing to an allocated
-array, then arraySize contains its size */
+				  /* If memPtr is pointing to an allocated
+				  array, then arraySize contains its size */
 	unsigned arraySize; // size of array
-	// Here, mPtr points to the allocated memory.
-	// If this is an array, then size specifies
-	// the size of the array.
+						// Here, mPtr points to the allocated memory.
+						// If this is an array, then size specifies
+						// the size of the array.
 
 	PtrDetails(T* _pnt, int _size = 0) :memPtr(_pnt), arraySize(_size)
 	{
-		//DONE
-
-		//initially set to false
+        //DONE
+        //initially set to false
 		isArray = false;
-
-		//set mem pointer to passed in val
+        
+        //keep track of the low level pointer
 		memPtr = _pnt;
-
-		//set size
+        
+		//initialize the size of array
 		arraySize = _size;
-
-		//initialize the refcount var
-		refcount = 1;
 
 		//if the passed in size is valid, set the array existence
 		//as being true
-		if (_size > 0)
-			isArray = true;
+		if (_size > 0){ 
+        	isArray = true;
+        }		
+
+		//initialize the refcount var
+		refcount = 1;
+		
 	}
 };
 // Overloading operator== allows two class objects to be compared.
 // This is needed by the STL list class.
 template <class T>
-bool operator==(const PtrDetails<T> &ob1,
-	const PtrDetails<T> &ob2)
+bool operator==(const PtrDetails<T> &ob1, const PtrDetails<T> &ob2)
 {
-	// DONE
+  // DONE
+  //return after comparison
 	return (ob1.memPtr == ob2.memPtr);
 }
